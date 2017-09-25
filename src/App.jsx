22e20e80c688c8a7d07f40b20e646875cbe4ff4f
ncleshyne <nicoleshayne.elem@gmail.com>
2017-09-25 16:58:15 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline});
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">
+        Countdown to {this.state.deadline}
+        </div>
+        <Clock
+          deadline={this.state.deadline}
+        />
+        <div>
+        <input
+          placeholder='new date'
+          onChange={event => this.setState({newDeadline: event.target.value})}
+        />
+        <button onClick={() => this.changeDeadline()}>
+        Submit
+        </button>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default App;
